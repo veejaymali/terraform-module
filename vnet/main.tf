adbb471e44b5c7463111a473e38d2e255d6cccc8
vijay <veejaymali@gmail.com>
2024-06-06 11:22:50 +0000

    added resource block

--- vnet/main.tf
+++ vnet/main.tf
@@ -30,3 +30,16 @@ locals {
     
   }
 }
+
+##
+#the block start with resource
+#type or resource which i want to creater
+#label unique
+resource "azurerm_resource_group" "myrg" {
+  #inside the resource block we provided argument
+  name = "${local.resource_name_prefix}-${var.resource_group_name}"
+  #sap-hr-rg-default
+  ##to call a variable we will use var.nameofthevariables
+  location = var.resource_group_location
+  tags     = local.common_tags
+}
